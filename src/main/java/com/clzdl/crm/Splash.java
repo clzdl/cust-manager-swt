4d359baa1372ea6b1127bef6ba4541483d6a4da2
java <java@ubuntu>
2019-03-06 22:10:44 +0800

    结构调整

--- src/main/java/com/clzdl/crm/Splash.java
+++ src/main/java/com/clzdl/crm/Splash.java
@@ -3,7 +3,7 @@ package com.clzdl.crm;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Shell;
 
-import com.clzdl.crm.common.AbstractSplash;
+import com.clzdl.crm.view.common.AbstractSplash;
 
 public class Splash extends AbstractSplash {
 	private MainWindow mainWindow;
--- src/main/java/com/clzdl/crm/view/common/AbstractSplash.java
+++ src/main/java/com/clzdl/crm/view/common/AbstractSplash.java
@@ -1,4 +1,4 @@
-package com.clzdl.crm.common;
+package com.clzdl.crm.view.common;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
