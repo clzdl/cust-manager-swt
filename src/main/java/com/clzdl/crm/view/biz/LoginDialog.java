6bd6fefbec104bf3a286940b31cf79975907c897
java <java@ubuntu>
2019-03-13 10:25:54 +0800

    功能完善

--- src/main/java/com/clzdl/crm/view/biz/LoginDialog.java
+++ src/main/java/com/clzdl/crm/view/biz/LoginDialog.java
@@ -1,5 +1,8 @@
 package com.clzdl.crm.view.biz;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -16,13 +19,26 @@ public class LoginDialog {
 	}
 
 	public Boolean show() {
-		dialog = new Shell(parent);
+		dialog = new Shell(parent, SWT.DIALOG_TRIM | SWT.PRIMARY_MODAL);
+		dialog.setSize(400, 300);
+		dialog.setText("登录");
+
+		dialog.addDisposeListener(new DisposeListener() {
+			@Override
+			public void widgetDisposed(DisposeEvent e) {
+				/// 关闭程序
+				parent.dispose();
+			}
+		});
+
 		/// 主屏幕显示位置
-		Rectangle bounds = display.getPrimaryMonitor().getBounds();
+		Rectangle bounds = parent.getBounds();
 		Rectangle rect = dialog.getBounds();
+//		Rectangle  = display.map(dialog, , dialogRect);
 		int x = bounds.x + Math.max(0, (bounds.width - rect.width) / 2);
 		int y = bounds.y + Math.max(0, (bounds.height - rect.height) / 2);
 		dialog.setBounds(x, y, rect.width, rect.height);
+
 		dialog.pack();
 		dialog.open();
 		while (!dialog.isDisposed()) {
