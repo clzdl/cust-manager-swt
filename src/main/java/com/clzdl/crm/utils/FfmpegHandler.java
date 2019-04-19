dd784435c8854605397bf5eee5e1412c4cc5a585
java <java@ubuntu>
2019-04-19 08:51:02 +0800

    功能完善

--- src/main/java/com/clzdl/crm/utils/FfmpegHandler.java
+++ src/main/java/com/clzdl/crm/utils/FfmpegHandler.java
@@ -10,7 +10,7 @@ import static org.bytedeco.ffmpeg.global.avcodec.avcodec_open2;
 import static org.bytedeco.ffmpeg.global.avcodec.avcodec_parameters_to_context;
 import static org.bytedeco.ffmpeg.global.avcodec.avcodec_receive_frame;
 import static org.bytedeco.ffmpeg.global.avcodec.avcodec_send_packet;
-import static org.bytedeco.ffmpeg.global.avformat.AVSEEK_FLAG_ANY;
+import static org.bytedeco.ffmpeg.global.avformat.AVSEEK_FLAG_BACKWARD;
 import static org.bytedeco.ffmpeg.global.avformat.av_dump_format;
 import static org.bytedeco.ffmpeg.global.avformat.av_read_frame;
 import static org.bytedeco.ffmpeg.global.avformat.av_seek_frame;
@@ -59,6 +59,21 @@ public class FfmpegHandler {
 	private Integer videoStreamIndex = -1;
 	private AVStream videoStream = null; /// 视频流
 	private AVCodecContext codecCtx = null;
+
+	public static interface GrabCallbak {
+		/**
+		 * 抓取一帧图片后的回调
+		 * 
+		 * @param image
+		 */
+		void showPictrue(BufferedImage image);
+
+		/**
+		 * 抓取完成后回调
+		 */
+		void grabDone();
+
+	}
 
 	/**
 	 * 打开多媒体文件流
@@ -124,22 +139,22 @@ public class FfmpegHandler {
 	 * @param gifDelayTime
 	 * @throws Exception
 	 */
-	public void grabPicture2Gif(int begSec, int endSec, String gifFile, Integer gifDelayTime, Integer frequency)
-			throws Exception {
-
+	public void grabPicture2Gif(int begSec, int endSec, String gifFile, Integer gifDelayTime, Integer frequency,
+			GrabCallbak cb) throws Exception {
+		long move2Pts = 0;
 		AnimatedGifEncoder encoderGif = new AnimatedGifEncoder();
 		AVFrame frm = av_frame_alloc();
 		// Allocate an AVFrame structure
 		AVFrame pFrameRGB = av_frame_alloc();
 		try {
 			encoderGif.setRepeat(0);
-			encoderGif.setDelay(gifDelayTime);
 			encoderGif.start(gifFile);
 
 			if (begSec > 0) {
-				begSec += videoStream.start_time();
-				int retSeek = av_seek_frame(fmtCtx, videoStreamIndex, (long) (begSec / av_q2d(videoStream.time_base())),
-						AVSEEK_FLAG_ANY);
+				move2Pts = (long) (begSec / av_q2d(videoStream.time_base()));
+				move2Pts += videoStream.start_time();
+				logger.info("move2Pts:{}", move2Pts);
+				int retSeek = av_seek_frame(fmtCtx, videoStreamIndex, move2Pts, AVSEEK_FLAG_BACKWARD);
 				if (retSeek < 0) {
 					logger.error("av_seek_frame fail");
 				}
@@ -174,15 +189,27 @@ public class FfmpegHandler {
 					// if not check ret2, error occur [swscaler @ 0x1cb3c40] bad src image pointers
 					// ret2 same as fi
 					if (ret2 >= 0) {
-						logger.info("qts:{}", frm.pts());
+						logger.info(
+								"frm:{}, duration (on base stream unit):{} ,move2Pts:{},(frm.pts() + frm.pkt_duration()):{}",
+								frm.pts(), frm.pkt_duration(), move2Pts, (frm.pts() + frm.pkt_duration()));
+						if (frm.pts() < move2Pts && (frm.pts() + frm.pkt_duration()) < move2Pts) {
+							continue;
+						}
+						logger.info("gif qts:{},duration:{}", frm.pts(), pkt.duration());
 						if (frequency >= 1 && cur++ % frequency != 0) {
 							continue;
 						}
 						sws_scale(sws_ctx, frm.data(), frm.linesize(), 0, codecCtx.height(), pFrameRGB.data(),
 								pFrameRGB.linesize());
-
-						encoderGif.addFrame(toImage(pFrameRGB, codecCtx.width(), codecCtx.height()));
-						if (pkt.pts() > endSecBaseStream) {
+						BufferedImage image = toImage(pFrameRGB, codecCtx.width(), codecCtx.height());
+						if (null != cb) {
+							cb.showPictrue(image);
+						}
+						encoderGif.addFrame(image);
+						int delay = gifDelayTime == null ? (int) (frm.pkt_duration() * av_q2d(videoStream.time_base()))
+								: gifDelayTime;
+						encoderGif.setDelay(delay);
+						if (frm.pts() > endSecBaseStream) {
 							break;
 						}
 					}
@@ -190,6 +217,9 @@ public class FfmpegHandler {
 				av_packet_unref(pkt);
 			}
 			encoderGif.finish();
+			if (null != cb) {
+				cb.grabDone();
+			}
 			logger.info("gif file build success");
 		} finally {
 			av_frame_free(frm);
@@ -240,14 +270,15 @@ public class FfmpegHandler {
 		return image;
 	}
 
-	public static void main(String[] args) throws Exception {
-		FfmpegHandler handler = new FfmpegHandler();
-		try {
-			handler.openMediaStream("/home/java/阳光电影www.ygdy8.com.检察方的罪人.BD.720p.日语中字.mkv");
-			handler.openVideoDecoder();
-			handler.grabPicture2Gif(120, 126, "/home/java/bbbbbbb.gif", 40, 5);
-		} finally {
-			handler.free();
-		}
-	}
+//	public static void main(String[] args) throws Exception {
+//		FfmpegHandler handler = new FfmpegHandler();
+//		try {
+//			// handler.openMediaStream("/home/java/阳光电影www.ygdy8.com.检察方的罪人.BD.720p.日语中字.mkv");
+//			handler.openMediaStream("/home/java/093006300116dbb404063082.mp4");
+//			handler.openVideoDecoder();
+//			handler.grabPicture2Gif(4, 5, "/home/java/bbbbbbb.gif", null, 1, null);
+//		} finally {
+//			handler.free();
+//		}
+//	}
 }
--- src/main/java/com/clzdl/crm/view/biz/panel/tool/content/Video2GIfContent.java
+++ src/main/java/com/clzdl/crm/view/biz/panel/tool/content/Video2GIfContent.java
@@ -1,15 +1,18 @@
 package com.clzdl.crm.view.biz.panel.tool.content;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DirectColorModel;
+import java.awt.image.IndexColorModel;
 import java.awt.image.WritableRaster;
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
@@ -23,49 +26,24 @@ import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.clzdl.crm.App;
 import com.clzdl.crm.springboot.auth.EnumSysPermissionProfile;
-import com.clzdl.crm.utils.ImageConvertor;
+import com.clzdl.crm.utils.FfmpegHandler;
+import com.clzdl.crm.utils.FfmpegHandler.GrabCallbak;
 import com.clzdl.crm.view.common.AbstractComposite;
-import com.clzdl.crm.view.common.LoadingDialog;
-import com.clzdl.crm.view.common.LoadingDialog.TaskLoading;
 import com.clzdl.crm.view.common.MsgBox;
 import com.framework.common.util.date.DateUtil;
-import com.madgag.gif.fmsware.AnimatedGifEncoder;
 
-import io.humble.video.Decoder;
-import io.humble.video.Demuxer;
-import io.humble.video.Demuxer.SeekFlag;
-import io.humble.video.DemuxerStream;
-import io.humble.video.Global;
-import io.humble.video.MediaDescriptor;
-import io.humble.video.MediaPacket;
-import io.humble.video.MediaPicture;
-import io.humble.video.Rational;
-import io.humble.video.awt.MediaPictureConverter;
-import io.humble.video.awt.MediaPictureConverterFactory;
-
-public class Video2GIfContent extends AbstractComposite {
+public class Video2GIfContent extends AbstractComposite implements GrabCallbak {
 	private final static Logger _logger = LoggerFactory.getLogger(Video2GIfContent.class);
-	private final static Integer _demuxer_in_buffer_size = 4 * 1024 * 1024;
 	private final static String title = "视频转Gif";
 	private DateTime dtEndTime;
 	private DateTime dtStartTime;
 	private Label videoPreview;
-	private BufferedImage currShowImage = null;
 	// 采样频率，数字越大，文件越小，丢失的帧越多，设置为1可保持原帧
 	private int frequency = 8;
-	private int count = 0;
-	private long recordStartFrmNumber = 0;
-	private long recordEndFrmNumber = 0;
-	private long leftRecordFrmNumber = 0;
 	private Text edtVideoFile;
 	private Text edtOutPath;
 	private Text edtFrequency;
-	private String gifFileName;
-	//// 解析到截取，同步使用
-	private CountDownLatch countDownLatch;
-	AnimatedGifEncoder encoderGif = new AnimatedGifEncoder();
 
 	public Video2GIfContent(Composite parent, int style) {
 		super(parent, style, title, EnumSysPermissionProfile.NONE);
@@ -148,26 +126,28 @@ public class Video2GIfContent extends AbstractComposite {
 				if (!edtFrequency.getText().trim().isEmpty()) {
 					frequency = Integer.valueOf(edtFrequency.getText().trim());
 				}
-				clear();
 				// 设置循环模式，0为无限循环 这里没有使用源文件的播放次数
-				encoderGif.setRepeat(0);
-				gifFileName = DateUtil.getCurrentDateTime() + ".gif";
-				encoderGif.start(edtOutPath.getText().trim() + "/" + gifFileName);
+				final String gifPathFile = edtOutPath.getText().trim() + "/" + DateUtil.getCurrentDateTime() + ".gif";
 				final String videoFile = edtVideoFile.getText().trim();
+				final FfmpegHandler handler = new FfmpegHandler();
 
 				new Thread(new Runnable() {
 					@Override
 					public void run() {
 						try {
-							convertVideo2Gif(videoFile, startDur, endDur);
-						} catch (InterruptedException | IOException e1) {
-							_logger.error(e1.getMessage(), e1);
+							handler.openMediaStream(videoFile);
+							handler.openVideoDecoder();
+							handler.grabPicture2Gif(startDur, endDur, gifPathFile, null, frequency,
+									Video2GIfContent.this);
+						} catch (Exception e) {
+							_logger.error(e.getMessage(), e);
+						} finally {
+							handler.free();
 						}
 					}
 				}).start();
 			}
 		});
-
 		FormData txtVideoFileFormData = new FormData();
 		txtVideoFileFormData.left = new FormAttachment(0, 10);
 		txtVideoFileFormData.top = new FormAttachment(0, 10);
@@ -245,205 +225,89 @@ public class Video2GIfContent extends AbstractComposite {
 		videoPreview.setLayoutData(videoPreviewFormData);
 	}
 
-	private void convertVideo2Gif(String videoFile, Integer startDur, Integer endDur)
-			throws InterruptedException, IOException {
-		countDownLatch = null;
-		countDownLatch = new CountDownLatch(1);
-
+	@Override
+	public void showPictrue(BufferedImage image) {
 		getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-				LoadingDialog loading = new LoadingDialog(App.getMainWindow(), App.getLoadingImgages());
-				loading.start(new TaskLoading() {
-
-					@Override
-					public void doing() {
-						try {
-							countDownLatch.await();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-				});
-			}
-		});
-
-		/*
-		 * Start by creating a container object, in this case a demuxer since we are
-		 * reading, to get video data from.
-		 */
-		Demuxer demuxer = Demuxer.make();
-		demuxer.setInputBufferLength(_demuxer_in_buffer_size);
-		/*
-		 * Open the demuxer with the filename passed on.
-		 */
-		demuxer.open(videoFile, null, false, true, null, null);
-
-		/*
-		 * Query how many streams the call to open found
-		 */
-		int numStreams = demuxer.getNumStreams();
-
-		/*
-		 * Iterate through the streams to find the first video stream
-		 */
-		int videoStreamId = -1;
-		long streamStartTime = Global.NO_PTS;
-		Decoder videoDecoder = null;
-		for (int i = 0; i < numStreams; i++) {
-			final DemuxerStream stream = demuxer.getStream(i);
-			streamStartTime = stream.getStartTime();
-			final Decoder decoder = stream.getDecoder();
-			if (decoder != null && decoder.getCodecType() == MediaDescriptor.Type.MEDIA_VIDEO) {
-				videoStreamId = i;
-				videoDecoder = decoder;
-				// stop at the first one.
-				break;
-			}
-		}
-		if (videoStreamId == -1)
-			throw new RuntimeException("could not find video stream in container: " + videoFile);
-
-		/*
-		 * Now we have found the video stream in this file. Let's open up our decoder so
-		 * it can do work.
-		 */
-		videoDecoder.open(null, null);
-		final MediaPicture picture = MediaPicture.make(videoDecoder.getWidth(), videoDecoder.getHeight(),
-				videoDecoder.getPixelFormat());
-		/**
-		 * A converter object we'll use to convert the picture in the video to a BGR_24
-		 * format that Java Swing can work with. You can still access the data directly
-		 * in the MediaPicture if you prefer, but this abstracts away from this demo
-		 * most of that byte-conversion work. Go read the source code for the converters
-		 * if you're a glutton for punishment.
-		 */
-		final MediaPictureConverter converter = MediaPictureConverterFactory
-				.createConverter(MediaPictureConverterFactory.HUMBLE_BGR_24, picture);
-		/**
-		 * This is the Window we will display in. See the code for this if you're
-		 * curious, but to keep this demo clean we're 'simplifying' Java AWT UI updating
-		 * code. This method just creates a single window on the UI thread, and blocks
-		 * until it is displayed.
-		 */
-
-		// All the MediaPicture objects decoded from the videoDecoder will share this
-		// timebase.
-		final Rational streamTimebase = videoDecoder.getTimeBase();
-		recordStartFrmNumber = convertDuration2PictureFrmNumber(streamTimebase, startDur);
-		recordEndFrmNumber = convertDuration2PictureFrmNumber(streamTimebase, endDur);
-		leftRecordFrmNumber = recordEndFrmNumber - recordStartFrmNumber + 1;
-
-		int ret = demuxer.seek(videoStreamId, 0, recordStartFrmNumber, recordEndFrmNumber,
-				SeekFlag.SEEK_FRAME.swigValue());
-		System.out.println("recordStartFrmNumber=" + recordStartFrmNumber + ",recordEndFrmNumber:" + recordEndFrmNumber
-				+ ",demuxer in buffer:" + demuxer.getInputBufferLength() + ",ret:" + ret);
-		countDownLatch.countDown();
-		/**
-		 * Now, we start walking through the container looking at each packet. This is a
-		 * decoding loop, and as you work with Humble you'll write a lot of these.
-		 * 
-		 * Notice how in this loop we reuse all of our objects to avoid reallocating
-		 * them. Each call to Humble resets objects to avoid unnecessary reallocation.
-		 */
-		final MediaPacket packet = MediaPacket.make();
-		while (demuxer.read(packet) >= 0) {
-			/**
-			 * Now we have a packet, let's see if it belongs to our video stream
-			 */
-			if (packet.getStreamIndex() == videoStreamId) {
-				/**
-				 * A packet can actually contain multiple sets of samples (or frames of samples
-				 * in decoding speak). So, we may need to call decode multiple times at
-				 * different offsets in the packet's data. We capture that here.
-				 */
-				int offset = 0;
-				int bytesRead = 0;
-				do {
-					bytesRead += videoDecoder.decode(picture, packet, offset);
-					if (picture.isComplete()) {
-						displayVideoAtCorrectTime(streamStartTime, picture, converter, streamTimebase);
-						if (0 >= leftRecordFrmNumber--) {
-							// 截取结束
-							break;
-						}
-					}
-					offset += bytesRead;
-				} while (offset < packet.getSize());
-				if (0 >= leftRecordFrmNumber) {
-					// 截取结束
-					break;
-				}
-			}
-		}
-
-		// Some video decoders (especially advanced ones) will cache
-		// video data before they begin decoding, so when you are done you need
-		// to flush them. The convention to flush Encoders or Decoders in Humble Video
-		// is to keep passing in null until incomplete samples or packets are returned.
-		do {
-			videoDecoder.decode(picture, null, 0);
-			if (picture.isComplete()) {
-				displayVideoAtCorrectTime(streamStartTime, picture, converter, streamTimebase);
-			}
-		} while (picture.isComplete());
-
-		// It is good practice to close demuxers when you're done to free
-		// up file handles. Humble will EVENTUALLY detect if nothing else
-		// references this demuxer and close it then, but get in the habit
-		// of cleaning up after yourself, and your future girlfriend/boyfriend
-		// will appreciate it.
-		demuxer.close();
-		encoderGif.finish();
-
-		getDisplay().asyncExec(new Runnable() {
-			@Override
-			public void run() {
-				new MsgBox(getShell(), "Gif文件[" + gifFileName + "]截取成功").open();
+				videoPreview.setImage(new Image(getDisplay(), convertToSWT(image)));
 			}
 		});
 	}
 
-	/**
-	 * Takes the video picture and displays it at the right time.
-	 */
-	private void displayVideoAtCorrectTime(long streamStartTime, final MediaPicture picture,
-			final MediaPictureConverter converter, final Rational streamTimebase) throws InterruptedException {
-		System.out.println(picture.getTimeStamp());
-		Long s = streamTimebase.rescale((long) (1000 * streamTimebase.getValue()), Rational.make(1, 24));
-		if (count++ % frequency == 0) {
-			// finally, convert the image from Humble format into Java images.
-			currShowImage = converter.toImage(currShowImage, picture);
-			// And ask the UI thread to repaint with the new image.
-			getDisplay().asyncExec(new Runnable() {
-				@Override
-				public void run() {
-					videoPreview.setImage(new Image(getDisplay(), ImageConvertor.convertToSWT(currShowImage)));
+	private ImageData convertToSWT(BufferedImage bufferedImage) {
+		if (bufferedImage.getColorModel() instanceof DirectColorModel) {
+			DirectColorModel colorModel = (DirectColorModel) bufferedImage.getColorModel();
+			PaletteData palette = new PaletteData(colorModel.getRedMask(), colorModel.getGreenMask(),
+					colorModel.getBlueMask());
+			ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(),
+					colorModel.getPixelSize(), palette);
+			WritableRaster raster = bufferedImage.getRaster();
+			int[] pixelArray = new int[3];
+			for (int y = 0; y < data.height; y++) {
+				for (int x = 0; x < data.width; x++) {
+					raster.getPixel(x, y, pixelArray);
+					int pixel = palette.getPixel(new RGB(pixelArray[0], pixelArray[1], pixelArray[2]));
+					data.setPixel(x, y, pixel);
 				}
-			});
-			encoderGif.addFrame(currShowImage);
+			}
+			return data;
+		} else if (bufferedImage.getColorModel() instanceof IndexColorModel) {
+			IndexColorModel colorModel = (IndexColorModel) bufferedImage.getColorModel();
+			int size = colorModel.getMapSize();
+			byte[] reds = new byte[size];
+			byte[] greens = new byte[size];
+			byte[] blues = new byte[size];
+			colorModel.getReds(reds);
+			colorModel.getGreens(greens);
+			colorModel.getBlues(blues);
+			RGB[] rgbs = new RGB[size];
+			for (int i = 0; i < rgbs.length; i++) {
+				rgbs[i] = new RGB(reds[i] & 0xFF, greens[i] & 0xFF, blues[i] & 0xFF);
+			}
+			PaletteData palette = new PaletteData(rgbs);
+			ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(),
+					colorModel.getPixelSize(), palette);
+			data.transparentPixel = colorModel.getTransparentPixel();
+			WritableRaster raster = bufferedImage.getRaster();
+			int[] pixelArray = new int[1];
+			for (int y = 0; y < data.height; y++) {
+				for (int x = 0; x < data.width; x++) {
+					raster.getPixel(x, y, pixelArray);
+					data.setPixel(x, y, pixelArray[0]);
+				}
+			}
+			return data;
+		} else if (bufferedImage.getColorModel() instanceof ComponentColorModel) {
+			ComponentColorModel colorModel = (ComponentColorModel) bufferedImage.getColorModel();
+			// ASSUMES: 3 BYTE BGR IMAGE TYPE
+			PaletteData palette = new PaletteData(0x0000FF, 0x00FF00, 0xFF0000);
+			ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(),
+					colorModel.getPixelSize(), palette);
+			// This is valid because we are using a 3-byte Data model with no transparent
+			// pixels
+			data.transparentPixel = -1;
+			WritableRaster raster = bufferedImage.getRaster();
+			int[] pixelArray = new int[3];
+			for (int y = 0; y < data.height; y++) {
+				for (int x = 0; x < data.width; x++) {
+					raster.getPixel(x, y, pixelArray);
+					int pixel = palette.getPixel(new RGB(pixelArray[0], pixelArray[1], pixelArray[2]));
+					data.setPixel(x, y, pixel);
+				}
+			}
+			return data;
 		}
-		System.out.println("s:" + s + ",streamBase:" + streamTimebase.toString());
-		Thread.sleep(s);
+		return null;
 	}
 
-	private BufferedImage deepCopy(BufferedImage bi) {
-		ColorModel cm = bi.getColorModel();
-		boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
-		WritableRaster raster = bi.copyData(null);
-		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
-	}
-
-	private long convertDuration2PictureFrmNumber(Rational streamTimebase, long duration) {
-		return (long) ((duration / streamTimebase.getValue())
-				/ (Rational.make(1, 24).getValue() / streamTimebase.getValue()));
-	}
-
-	private void clear() {
-		count = 0;
-		recordStartFrmNumber = 0;
-		recordEndFrmNumber = 0;
-		leftRecordFrmNumber = 0;
-		gifFileName = "";
+	@Override
+	public void grabDone() {
+		getDisplay().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				new MsgBox(getShell(), "抓取完成").open();
+			}
+		});
 	}
 }
